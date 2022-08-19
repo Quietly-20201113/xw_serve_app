d4b89500adfc8c30f1edb42a61542c1abf278b8c
山川 <18487115313@163.com>
2022-08-19 16:44:59 +0800

    【添加】 添加控制器，处理数据连接

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const userController = require('../controllers/userController');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// 获取用户信息
+router.get('/get_user', userController.showUser);
 
 module.exports = router;
